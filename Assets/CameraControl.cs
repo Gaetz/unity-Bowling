f16ed6c3874cbf5966bd0bd927af3bd713c0a86f
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-05-10 14:12:23 +0200

    Add camara script

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+
+    public Ball ball;
+
+    private Vector3 ballOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
