7767490dbe9a2d4babd0ed0a68f7e749f25c7f9d
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-05-10 11:27:04 +0200

    Launch ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    /// <summary>
+    /// Launch ball speed
+    /// </summary>
+    public float LaunchSpeed;
+
+    private Rigidbody rigidBody;
+    private AudioSource ballAudioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        ballAudioSource = GetComponent<AudioSource>();
+        LaunchBall();
+    }
+
+    private void LaunchBall()
+    {
+        // Initial speed
+        rigidBody.velocity = new Vector3(0, 0, LaunchSpeed);
+        // Sound
+        ballAudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
