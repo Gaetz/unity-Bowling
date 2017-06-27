a32c075881c7259c36a6675e5a47e6cdffdb9474
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-06-27 12:28:38 +0200

    Launching the ball with touch/mouse input (with files)

---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class DragLaunch : MonoBehaviour {
+
+    private Ball ball;
+    private Vector3 startPosition;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        ball = GetComponent<Ball>();
+	}
+
+    public void DragStart()
+    {
+        // Capture time and position of drag start
+        startPosition = Input.mousePosition;
+        startTime = Time.time;
+    }
+
+    public void DragEnd()
+    {
+        // Calculate velocity
+        float launchDuration = Time.time - startTime;
+        float launchVelocityX = (Input.mousePosition.x - startPosition.x) / launchDuration;
+        float launchVelocityZ = (Input.mousePosition.y - startPosition.y) / launchDuration;
+
+        // Launch ball
+        ball.LaunchBall(new Vector3(launchVelocityX, 0, launchVelocityZ * 2));
+    }
+}
