e1f8edac557bccf1a0624d38298a70f2363abf59
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-06-30 16:54:48 +0200

    Add a swiper for pins and a pin upright check

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float StandingThreshold;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool IsStanding()
+    {
+        return Mathf.Abs(transform.rotation.eulerAngles.x) < StandingThreshold
+            && Mathf.Abs(transform.rotation.eulerAngles.z) < StandingThreshold;
+    }
+}
