48e6035c54115b249c7c27dced188783c925f4f3
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-07-13 12:02:07 +0200

    Unit test for scoremaster

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System;
+
+
+[TestFixture]
+public class ActionMasterTests
+{
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster actionMaster;
+
+    [SetUp]
+    public void Setup()
+    {
+        actionMaster = new ActionMaster();
+    }
+
+    [Test]
+    public void T00_PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01_OneStrikeReturnEndTurn()
+    {
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T02_8ReturnTidy()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03_28SpareReturnEndTurn()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(2));
+        Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+    }
+
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+    public enum Action
+    {
+        Tidy,
+        Reset,
+        EndTurn,
+        EndGame
+    }
+
+    //public int[] bowls = new int[21];
+    public int bowlIndex = 1;
+
+    public Action Bowl(int pins)
+    {
+        // Guard
+        if(pins < 0 || pins > 10)
+        {
+            throw new Exception("Pin number should be between 0 and 10, both include.");
+        }
+        // Behaviour
+        if (pins == 10)
+        {
+            bowlIndex += 2;
+            return Action.EndTurn;
+        }
+        else
+        {
+            // End frame
+            if(bowlIndex % 2 == 0)
+            {
+                bowlIndex += 1;
+                return Action.EndTurn;
+            }
+            // Mid frame or last frame
+            else
+            {
+                bowlIndex += 1;
+                return Action.Tidy;
+            }
+        }
+        // Other Behaviour
+        throw new Exception("Should return an action, but don't know which.");
+    }
+}
