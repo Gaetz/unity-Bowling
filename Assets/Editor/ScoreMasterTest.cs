44af197c9d88cc97a7370c7dfa863cef5de91fed
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-07-13 10:18:59 +0200

    First unit test

---
+++ Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System;
+
+
+[TestFixture]
+public class ScoreTests
+{
+    [Test]
+    public void PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
