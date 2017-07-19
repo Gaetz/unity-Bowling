a0be91066d4aed910d075f0e3fb94042dc885c77
Gaetz <gaetz@rufflerim.com>
2017-07-19 05:58:14 +0200

    Score display

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] rollTexts;
+    public Text[] frameTexts;
+
+    // Use this for initialization
+    void Start () {
+        foreach (Text display in rollTexts)
+            display.text = "";
+        foreach (Text display in frameTexts)
+            display.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
